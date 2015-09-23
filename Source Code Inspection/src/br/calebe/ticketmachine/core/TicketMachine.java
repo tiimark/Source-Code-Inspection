124e0e69f2d51d97907d0671113fc746f042b2da
31467253 <31467253@087859-p33s105.macklabs.br>
2015-09-23 10:32:46 -0300

    protected trocado por private papelMoeda vira final papelMoeda[1] trocado para papelMoeda[i]

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -19,17 +19,17 @@ public class TicketMachine {
         this.saldo = 0;
     }
 
-    public void inserir(int quantiaInserida) throws PapelMoedaInvalidaException {
+    public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[i] == quantiaInserida) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
             }
         }
         if (!achou) {
             throw new PapelMoedaInvalidaException();
         }
-        this.saldo += quantiaInserida;
+        this.saldo += quantia;
     }
 
     public int getSaldo() {
